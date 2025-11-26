12fcaf12985b4910fbb7e04a63a27598e5c88f42
Abu Sayem Sifat <sifatabusayem@gmail.com>
2025-11-26 11:31:56 +0600

    added header and footer, services section is in progress

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -5,13 +5,17 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+import c1 from '../assets/cat1.jpg'
+import c2 from '../assets/cat2.jpg'
+import c3 from '../assets/cat3.jpg'
+
 const Slider = () => {
     return (
         <div>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide><img src="" alt="" /></SwiperSlide>
-                <SwiperSlide><img src="" alt="" /></SwiperSlide>
-                <SwiperSlide><img src="" alt="" /></SwiperSlide>
+                <SwiperSlide><img className='w-full h-[600px] object-cover object-center' src={c1} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='w-full h-[600px] object-cover object-center' src={c2} alt="" /></SwiperSlide>
+                <SwiperSlide><img className='w-full h-[600px] object-cover object-center' src={c3} alt="" /></SwiperSlide>
             </Swiper>
         </div>
     );
