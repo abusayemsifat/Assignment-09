afa3b51314131a2c27f99485c88b9d1d103c499c
Abu Sayem Sifat <sifatabusayem@gmail.com>
2025-11-26 19:25:06 +0600

    added winter care tips section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from '../Components/Slider';
 import PopularSection from '../Components/PopularSection';
 import MeetOurVets from '../Components/MeetOurVets';
+import WinterCareTips from '../Components/WinterCareTips';
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Slider></Slider>
             <div className='w-11/12 mx-auto'>
                 <PopularSection></PopularSection>
+                <WinterCareTips></WinterCareTips>
                 <MeetOurVets></MeetOurVets>
             </div>
         </div>
